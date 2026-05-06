171e9c43854750073207717715f24df722e5c0fc
vishal kumar <vishaljpkumar4970@gmail.com>
2026-05-06 17:41:06 +0530

    ssd

--- backend/routes/hotelsRoutes.js
+++ backend/routes/hotelsRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express'
 
 import { hotels } from '../controllers/hotelControllers.js'
-let router=express.Router()
-
+let router=express.Router();
 
 router.post("/hotels",hotels)
 
-export default router
+export default router;
