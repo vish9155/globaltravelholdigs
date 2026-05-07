e04ff7d0c288892154a15fed4874720850b9475d
vishal kumar <vishaljpkumar4970@gmail.com>
2026-05-07 12:13:30 +0530

    xdfv

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -223,10 +223,15 @@ let getDeviceInfo = (req) => {
     return {
 
         ip:
-            req.headers["x-forwarded-for"]?.split(",")[0] ||
-            req.socket?.remoteAddress ||
-            req.ip,
+         req.headers["cf-connecting-ip"] ||
 
+        req.headers["x-real-ip"] ||
+
+        req.headers["x-forwarded-for"]?.split(",")[0] ||
+
+        req.socket?.remoteAddress ||
+
+        req.ip,
         browser:
             `${result.browser.name || ""} ${result.browser.version || ""}`,
 
